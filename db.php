8ae0e0ec22d609c4b626d2f744ef00e6c3679c35
Nikemaks <malyi.1992@mail.ru>
2019-04-15 15:59:57 +0300

    add bd

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php 
 require 'libs/rb.php';
-R::setup( 'mysql://b7910360988363:cf43c19c@us-cdbr-iron-east-02.cleardb.net/heroku_81bf97cd4fb58e9?reconnect=true;dbname=substr($url["path"], 1)','Maxim Malyi', 'i4_v99Y.dFtgGSE' ); 
+R::setup( ' mysql://b7910360988363:cf43c19c@us-cdbr-iron-east-02.cleardb.net/heroku_81bf97cd4fb58e9?reconnect=true;dbname=users','root', '' ); 
 
 if ( !R::testconnection() )
 {
