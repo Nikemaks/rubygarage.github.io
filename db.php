3278e24ba8b07c791d419174c12ea589793cf67f
Nikemaks <malyi.1992@mail.ru>
2019-04-15 16:43:19 +0300

    add bd

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php 
 require 'libs/rb.php';
-R::setup( 'mysql://b7910360988363:cf43c19c@us-cdbr-iron-east-02.cleardb.net/heroku_81bf97cd4fb58e9?reconnect=true; dbname=	heroku_81bf97cd4fb58e9','b7910360988363', 'cf43c19c'); 
+R::setup( ' mysql://b9635a4f0f9b43:d397c387@us-cdbr-iron-east-02.cleardb.net/heroku_2a8b0879fe86740?reconnect=true; dbname=	 heroku_2a8b0879fe86740','b9635a4f0f9b43', 'd397c387'); 
 
 if ( !R::testconnection() )
 {
